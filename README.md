d0628012dde00e35510eb80c766ab4235e45ef30
Buck Ryan <fryan@rjmetrics.com>
2015-01-10 17:43:36 -0500

    update README and change stand.ino to only print the distance

--- README.md
+++ README.md
@@ -2,3 +2,48 @@ standuino
 =========
 
 Code to monitor an arduino for sitting / standing
+
+Prerequisites
+=============
+
+```
+apt-get install arduino-core
+apt-get install picocom
+pip install ino
+```
+
+Setup Arduino
+=============
+
+Check out the sketch
+[here](http://arduinobasics.blogspot.com/2012/11/arduinobasics-hc-sr04-ultrasonic-sensor.html).
+
+To give a brief rundown, the ultrasonic sensor needs four connections. You will
+see labels for each pin on the front of the sensor. These are named
+
+* `VCC` for voltage
+* `Trig` for trigger
+* `Echo`
+* `GND` for ground
+
+Connect `VCC` pin to 5V power. Connect ground to ground. Connect 7 to `Echo`
+and connect 8 to `Trig`.
+
+
+Build and load
+==============
+
+```
+cd arduino
+ino build
+ino upload
+```
+
+Serial
+======
+
+```
+ino serial
+```
+
+To quit: `<C-a><C-q>`
--- arduino/src/stand.ino
+++ arduino/src/stand.ino
@@ -1,29 +1,9 @@
-const int RED = 2;
-const int GREEN = 4;
 const int ECHO = 7;
 const int TRIGGER = 8;
-
-const int DISTANCE_THRESHOLD_CM = 135;
-
-boolean _standing = false;
-boolean _initialized = false;
-
-void setStanding(boolean standing) {
-  _standing = standing;
-
-  int gVal = _standing ? HIGH : LOW; // value of the green pin
-  digitalWrite(GREEN, gVal);
-  digitalWrite(RED, (gVal + 1) % 2);
-
-  Serial.print("event: ");
-  Serial.println(_standing ? "standing" : "sitting");
-}
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(RED, OUTPUT);
-  pinMode(GREEN, OUTPUT);
   pinMode(ECHO, INPUT);
   pinMode(TRIGGER, OUTPUT);
 }
@@ -32,7 +12,7 @@ void setup() {
  * code for reading the ultrasonic sensor based on:
  * http://arduinobasics.blogspot.com/2012/11/arduinobasics-hc-sr04-ultrasonic-sensor.html
  */
-int readSensor() {
+int readDistanceInCm() {
   digitalWrite(TRIGGER, LOW);
   delayMicroseconds(2);
 
@@ -42,25 +22,22 @@ int readSensor() {
   digitalWrite(TRIGGER, LOW);
   int duration = pulseIn(ECHO, HIGH);
 
+  // not sure what 58.2 is - the article above indicates the distance
+  // calculated is in cm based on the speed of sound
   int distance = duration / 58.2;
 
   return distance;
 }
 
 void checkStatus() {
-  int distance = readSensor();
+  int distance = readDistanceInCm();
 
   if(distance < 0)
     return;
 
-  Serial.print("distance: ");
-  Serial.println(distance);
-
-  boolean standing = distance < DISTANCE_THRESHOLD_CM;
-  if(!_initialized || _standing != standing) {
-    setStanding(standing);
-    _initialized = true;
-  }
+  Serial.print("{\"distance_cm\": ");
+  Serial.print(distance);
+  Serial.println("}");
 }
 
 void loop() {
